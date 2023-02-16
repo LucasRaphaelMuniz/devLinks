5e191547024e37d1d0d62213a1f5dac0050efd35
Lucas Muniz <lucasrmuniz@outlook.com.br>
2023-02-15 21:19:44 -0300

    update photo perfil

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "src",
+      "https://avatars.githubusercontent.com/u/114521141?v=4"
+    )
     img.setAttribute("alt", "Foto de perfil light-mode")
-  } else {
-    img.setAttribute("src", "https://avatars.githubusercontent.com/u/114521141?v=4")
+  } else {    
+    img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute("alt", "Foto de perfil dark-mode")
   }
 
